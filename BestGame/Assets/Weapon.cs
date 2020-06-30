a546c85d21a8654a928aa4357f639ecc306fd341
aryan040501 <49898427+aryan040501@users.noreply.github.com>
2020-06-30 18:58:52 +0530

    first commit

---
+++ BestGame/Assets/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject BulletPrefab;
+    public float bulletSpeed = 20f;
+    
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(BulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb2d = bullet.GetComponent<Rigidbody2D>();
+        rb2d.AddForce(firePoint.right * bulletSpeed, ForceMode2D.Impulse);
+
+    }
+
+    
+}
---
+++ BestGame/Assets/move2D.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move2D : MonoBehaviour
+{
+    
+    public Rigidbody2D rb2d;
+    public float jumpForce = 5f;
+    public float moveSpeed = 10f;
+
+    public Animator animator;
+    private bool facingRight = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+        //flip
+        if (Input.GetAxis("Horizontal") < 0 && facingRight)
+        {
+            flip();
+        }
+        if (Input.GetAxis("Horizontal") > 0 && !facingRight)
+        {
+            flip();
+        }
+        
+    }
+
+    void Jump()
+    {
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+        
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+        
+   
+}
